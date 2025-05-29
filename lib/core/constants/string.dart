e11854c4098cf6adb16f652d50f012d35609a163
Himanshu325437 <himanshu@aspiresoftwareconsultancy.com>
2025-05-29 17:51:41 +0530

    first commit

---
+++ lib/core/constants/string.dart
@@ -0,0 +1,19 @@
+class AppStrings {
+  // Private constructor
+  AppStrings._();
+
+  // Singleton instance
+  static final AppStrings instance = AppStrings._();
+
+  // General
+  final String appName = "Himanshu Singh | Mobile Developer";
+  
+
+
+  // Error messages
+  final String errorGeneral = "Something went wrong. Please try again.";
+  final String errorNetwork = "No internet connection.";
+  
+
+  // Add more strings as needed...
+}
---
+++ lib/core/theme/color_pallete.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  ///// Light Theme //////////////////////
+  static const Color primary = Color(0xFF0369A1); // Confirm button green
+  static const Color secondary =  Color(0xFF2ABFB9); 
+  static const Color scaffoldBackgroundColor = Colors.white;
+  static const Color background = Colors.white; // Light background
+  static const Color secondaryBackground = Color(0xffF9FAFB);
+  static const Color textPrimary = Color(0xFF121212); // Main black text
+  static const Color textSecondary = Color(0xFFC6C7C9); // Subtle gray text
+  static const Color textTernary = Color(0xFFA8A9AB); // Subtle gray text
+  static const Color border = Color(0xFFE5E7EB); // Light gray borders
+  static const Color buttons = Color(0xFFF5F8FD); // Light gray borders
+  static const Color accent = Color(0xFF000000); // Bold black for selection
+  static const Color disabled = Color(0xFFF5F6FA);
+}

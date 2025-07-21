e36c142c542fca20b73fcf72c2f3b8939d6afc48
Himanshu Personal <singh.himanshu04051999@gmail.com>
2025-07-22 01:35:41 +0530

    remove .env

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:portfolio/feature/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
